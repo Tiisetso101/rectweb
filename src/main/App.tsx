07a88def859ba79740a5aebc50e67c7c4c45d7ac
HOLLYWOODBETS\TiisetsoM <tiisetsom@hollywoodbets.net>
2023-11-25 13:33:33 +0200

    first commit

---
+++ src/main/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './src/logo.png';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="container">
+     <Header subtitle="Providing Houses all over the world"/>
+    </div>
+  );
+}
+
+export default App;
